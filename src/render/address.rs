f51210cb18bfa6539df6c15fe5bc3fc203b6b392
Riccardo Casatta <riccardo@casatta.it>
2023-02-25 09:59:10 +0100

    Add link to addresses

--- src/render/address.rs
+++ src/render/address.rs
@@ -1,12 +1,17 @@
 use super::Html;
+use crate::{network, NetworkExt};
 use maud::{html, Render};
 
 pub(crate) struct Address<'a>(&'a bitcoin::Address);
 
 impl<'a> Render for Address<'a> {
     fn render(&self) -> maud::Markup {
+        let address_string = self.0.to_string();
+        let network_url_path = network().as_url_path();
+        let link = format!("{network_url_path}a/{address_string}");
+
         html! {
-             code { (self.0.to_string()) }
+            a href=(link) {  code { (address_string) } }
         }
     }
 }
