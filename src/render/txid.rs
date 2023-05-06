5b0a44173a5d14088d087ef6c352cc1099398a0d
Riccardo Casatta <riccardo@casatta.it>
2023-05-06 23:31:07 +0200

    use Display in Txid render

--- src/render/txid.rs
+++ src/render/txid.rs
@@ -1,13 +1,22 @@
+use std::fmt::Display;
+
 use super::Html;
-use crate::{globals::network, NetworkExt};
+use crate::{globals::network, NetworkExt, NetworkPath};
 use maud::{html, Render};
 
 pub(crate) struct Txid(bitcoin::Txid);
 
+struct Link(NetworkPath, bitcoin::Txid);
+impl Display for Link {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}t/{:x}", self.0, self.1)
+    }
+}
+
 impl Render for Txid {
     fn render(&self) -> maud::Markup {
         let network_url_path = network().as_url_path();
-        let link = format!("{network_url_path}t/{:x}", self.0);
+        let link = Link(network_url_path, self.0);
 
         html! {
             a href=(link) { code { small { u { (self.0) } } } }
