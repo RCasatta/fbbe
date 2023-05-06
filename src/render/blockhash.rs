11ba1a1172b54040377b8a72526062b472ff2abc
Riccardo Casatta <riccardo@casatta.it>
2023-05-06 23:33:52 +0200

    use Display in Blockhash render

--- src/render/blockhash.rs
+++ src/render/blockhash.rs
@@ -1,17 +1,25 @@
+use std::fmt::Display;
+
 use super::Html;
-use crate::{globals::network, NetworkExt};
+use crate::{globals::network, NetworkExt, NetworkPath};
 use maud::{html, Render};
 
 pub(crate) struct BlockHash(pub bitcoin::BlockHash);
 
+struct Link(NetworkPath, bitcoin::BlockHash);
+impl Display for Link {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}b/{:x}", self.0, self.1)
+    }
+}
+
 impl Render for BlockHash {
     fn render(&self) -> maud::Markup {
-        let hex = format!("{:x}", self.0);
         let network_url_path = network().as_url_path();
-        let link = format!("{network_url_path}b/{hex}");
+        let link = Link(network_url_path, self.0);
 
         html! {
-            a href=(link) { code { small { (hex) } } }
+            a href=(link) { code { small { (self.0) } } }
         }
     }
 }
