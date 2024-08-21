fe55de7d419b32bd5bf9affe332aece65ec15237
Riccardo Casatta <riccardo@casatta.it>
2024-08-21 23:22:36 +0200

    env var to avoid logging timestamp

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
 use clap::Parser;
 use env_logger::Env;
 use fbbe::{inner_main, Arguments};
+use std::io::Write;
 
 #[tokio::main]
 async fn main() {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let mut builder = env_logger::Builder::from_env(Env::default().default_filter_or("info"));
+    if std::env::var("LOG_AVOID_TIMESTAMP").is_ok() {
+        builder.format(|buf, record| {
+            writeln!(
+                buf,
+                "{:5} {} {}",
+                record.level(),
+                record.target(),
+                record.args()
+            )
+        });
+    }
+
+    builder.init();
     let args = Arguments::parse();
 
     if let Err(e) = inner_main(args).await {
