922d3c92295c8b2ff2d1db2cf01b8f9fe8d48ce4
Riccardo Casatta <riccardo@casatta.it>
2025-01-10 14:32:01 +0100

    fix systemd logging init

--- src/main.rs
+++ src/main.rs
@@ -13,26 +13,20 @@ static GLOBAL: Jemalloc = Jemalloc;
 #[tokio::main]
 async fn main() {
     let mut builder = env_logger::Builder::from_env(Env::default().default_filter_or("info"));
-    match std::env::var("RUST_LOG_STYLE") {
-        Ok(s) if s == "SYSTEMD" => builder
-            .format(|buf, record| {
-                writeln!(
-                    buf,
-                    "<{}>{}: {}",
-                    match record.level() {
-                        log::Level::Error => 3,
-                        log::Level::Warn => 4,
-                        log::Level::Info => 6,
-                        log::Level::Debug => 7,
-                        log::Level::Trace => 7,
-                    },
-                    record.target(),
-                    record.args()
-                )
-            })
-            .init(),
-        _ => (),
-    };
+    if let Ok(s) = std::env::var("RUST_LOG_STYLE") {
+        if s == "SYSTEMD" {
+            builder.format(|buf, record| {
+                let level = match record.level() {
+                    log::Level::Error => 3,
+                    log::Level::Warn => 4,
+                    log::Level::Info => 6,
+                    log::Level::Debug => 7,
+                    log::Level::Trace => 7,
+                };
+                writeln!(buf, "<{}>{}: {}", level, record.target(), record.args())
+            });
+        }
+    }
 
     builder.init();
     let args = Arguments::parse();
